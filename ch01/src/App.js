649035a924c207234eba3582304bfccb9ea3f3ce
vudinhquang <vudinhquangk53@gmail.com>
2019-12-29 23:14:30 +0900

    App

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello</h2>
-      </header>
+    <div className="row">
+      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <div className="panel panel-info">
+          <div className="panel-heading">ReactJs</div>
+          <div className="panel-body">
+            <ul className="list-group">
+              <li className="list-group-item">Item 1</li>
+              <li className="list-group-item">Item 2</li>
+              <li className="list-group-item">Item 3</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- ch01/src/index.js
+++ ch01/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Hello from './Hello';
+import App from './App';
+// import Hello from './components/Hello';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Hello />, document.getElementById('main-content'));
+ReactDOM.render(<App />, document.getElementById('main-content'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
