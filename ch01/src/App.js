3fb2918d0b0cc7d3ffb65fdd08760526c059b6c9
vudinhquang <vudinhquangk53@gmail.com>
2019-12-30 01:33:28 +0900

    Props

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -5,9 +5,9 @@ import Course from './components/Course'
 function App() {
   return (
     <div className="row">
-      <Course></Course>
-      <Course />
-      <Course />
+      <Course name="ReactJS" time="30h" free={true}>ReactJS is very simple</Course>
+      <Course name="Angular 4x" time="50h" free={false} />
+      <Course name="NodeJS" time="35h" free={true} />
     </div>
   );
 }
--- ch01/src/components/Course.js
+++ ch01/src/components/Course.js
@@ -8,22 +8,35 @@ function formatName(user) {
 const user = {
   firstName: 'Harper',
   lastName: 'Perez'
-};
+}
 
-function Course() {
+function showButtonFree(props) {
+  const isFree = props.free;
+  console.log("isFree", isFree);
+  if(isFree === true) {
+    return <div className="panel-footer">
+            <button type="button" className="btn btn-warning">view</button>
+          </div>
+  }
+}
+
+function Course(props) {
   return (
     <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
       <div className="panel panel-info">
-        <div className="panel-heading">ReactJs</div>
+        <div className="panel-heading">{ props.name }</div>
         <div className="panel-body">
           { formatName(user) }
           { 2 + 2 }
+          <p>{ props.time }</p>
+          <p>{ props.children }</p>
           <ul className="list-group">
             <Lesson />
             <Lesson />
             <Lesson />
           </ul>
         </div>
+        { showButtonFree(props) }
       </div>
     </div>
   );
