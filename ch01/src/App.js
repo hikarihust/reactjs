a9966357c2e156fa383bb6f895bcc61f96fd84b8
vudinhquang <vudinhquangk53@gmail.com>
2019-12-29 23:27:36 +0900

    Course

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import './App.css';
+import Course from './components/Course'
 
 function App() {
   return (
     <div className="row">
-      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-        <div className="panel panel-info">
-          <div className="panel-heading">ReactJs</div>
-          <div className="panel-body">
-            <ul className="list-group">
-              <li className="list-group-item">Item 1</li>
-              <li className="list-group-item">Item 2</li>
-              <li className="list-group-item">Item 3</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <Course></Course>
+      <Course />
+      <Course />
     </div>
   );
 }
---
+++ ch01/src/components/Course.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Course() {
+  return (
+    <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <div className="panel panel-info">
+        <div className="panel-heading">ReactJs</div>
+        <div className="panel-body">
+          <ul className="list-group">
+            <li className="list-group-item">Item 1</li>
+            <li className="list-group-item">Item 2</li>
+            <li className="list-group-item">Item 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Course;
