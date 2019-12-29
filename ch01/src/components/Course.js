b37d9baeceb3e3f05a17e53bcca9205423f6aa2c
vudinhquang <vudinhquangk53@gmail.com>
2019-12-30 00:33:58 +0900

    JSX

--- ch01/src/components/Course.js
+++ ch01/src/components/Course.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Lesson from './Lesson'
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
 function Course() {
   return (
     <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
       <div className="panel panel-info">
         <div className="panel-heading">ReactJs</div>
         <div className="panel-body">
+          { formatName(user) }
+          { 2 + 2 }
           <ul className="list-group">
             <Lesson />
             <Lesson />
