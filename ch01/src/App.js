f0050c285f583433fa52bb92c96885de74c9b299
vudinhquang <vudinhquangk53@gmail.com>
2019-12-31 00:36:51 +0900

    LifeCycle

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,36 +1,51 @@
-import React from 'react';
+// import React from 'react';
 import './App.css';
-import Course from './components/Course'
+import Course from './components/Course';
+import React, { Component } from 'react';
+import Lifecycle from './components/Lifecycle';
 
-function App() {
-  const items = [
-    {
-      name: 'ReactJS',
-      time: '30h',
-      free: false,
-      desc: 'ReactJS is very simple'
-    },
-    {
-      name: 'Angular 4x',
-      time: '50h',
-      free: true,
-    },
-    {
-      name: 'NodeJS',
-      time: '35h',
-      free: true
-    }
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  const elmCourses = items.map((item, index) =>
-    <Course key={ index } name={ item.name } time={ item.time } free={ item.free }>{ item.desc }</Course>
-  );
+    this.state = {
+        items: [
+            {
+                name: 'ReactJS',
+                time: '30h',
+                free: false,
+                desc: 'ReactJS is very simple!'
+            },
+            {
+                name: 'Angular 4x',
+                time: '55h',
+                free: true,
+            },
+            {
+                name: 'NodeJS',
+                time: '35h',
+                free: true,
+            }
+        ]
+    };
+}
 
-  return (
-    <div className="row">
-      { elmCourses }
-    </div>
-  );
+
+  render() {
+  
+    let elmCourses = this.state.items.map((item, index) =>
+      <Course key={ index } name={ item.name } time={ item.time } free={ item.free }>{ item.desc }</Course>
+    );
+
+    elmCourses = null;
+    
+    return (
+      <div className="row">
+        { elmCourses }
+        <Lifecycle />
+      </div>
+    )
+  }
 }
 
 export default App;
