14eb9a005e3ee1bbb73d93ce9c5cbbe4891cfaf5
vudinhquang <vudinhquangk53@gmail.com>
2020-01-05 19:03:04 +0900

    Publish ứng dụng

---
+++ ch02/todolist/precache-manifest.0739131b8a10bad603e8a9436759456a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b503caede0bb51cd0df7d6ee7bf3c124",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1e6edfdd4d531d54f4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cc11726eacdafcba1661",
+    "url": "/static/js/2.2ce031b4.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/static/js/2.2ce031b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d1e6edfdd4d531d54f4",
+    "url": "/static/js/main.fbb0954a.chunk.js"
+  },
+  {
+    "revision": "1c2a6b19461853a026a6",
+    "url": "/static/js/runtime-main.23f53c1c.js"
+  }
+]);
