d0f5031a0606f26b0d8575641b4a3e8ae4567027
vudinhquang <vudinhquangk53@gmail.com>
2020-01-09 00:32:34 +0900

    fix error

--- ch04/src/components/ProductItem.js
+++ ch04/src/components/ProductItem.js
@@ -13,7 +13,7 @@ class ProductItem extends Component {
         return (
             <div className="media product">
                 <div className="media-left">
-                    <a href="#">
+                    <a href="http://localhost:3000/">
                         <img className="media-object" src="images/aplusautomation.jpg" alt="charmander" />
                     </a>
                 </div>
@@ -21,7 +21,7 @@ class ProductItem extends Component {
                     <h4 className="media-heading">aplusautomation</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dicta asperiores veniam repellat unde debitis quisquam magnam magni ut deleniti!</p>
                     <input name="quantity-product-1" type="number" defaultValue={1} min={1} />
-                    <a data-product={1} href="#" className="price"> 12 USD </a>
+                    <a data-product={1} type="button" className="price"> 12 USD </a>
                 </div>
             </div>
         );
