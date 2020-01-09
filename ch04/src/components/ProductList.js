5af0009ca2263b2ea12862479beaf42d799b774e
vudinhquang <vudinhquangk53@gmail.com>
2020-01-09 21:26:06 +0900

    ProductList

--- ch04/src/components/ProductList.js
+++ ch04/src/components/ProductList.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import * as configs from './../constants/Config'
 import ProductItem from './ProductItem';
 import ProductItemNoSale from './ProductItemNoSale';
 
 class ProductList extends Component {
 
     render() {
+        let { products } = this.props;
         return (
             <div className="col-xs-12 col-sm-12 col-md-5 col-lg-5">
                 <div className="panel panel-primary"> 
                     <div className="panel-heading"><h1 className="panel-title">List Products</h1></div> 
                     <div className="panel-body" id="list-product">
-						<ProductItem />
+						{ this.showElementProduct(products) }
 						<ProductItemNoSale />
                     </div>
                 </div>
             </div>
         );
     }
+
+    showElementProduct(products) {
+        let xhtml = <b>{ configs.NOTI_EMPTY_PRODUCT }</b>
+
+        if (products.length > 0) {
+            xhtml = products.map((product, index) => {
+                return (
+                    <ProductItem key={ index } product={ product } index={ index } />
+                );
+            });
+        }
+
+        return xhtml;
+    }
   
 }
 
-export default ProductList;
+const mapStateToProps = state => {
+    return {
+        products: state.products
+    }
+}
+
+export default connect(mapStateToProps, null)(ProductList);
