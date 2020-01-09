ac7e4eae505835870da95bbad2c297c158a87b5e
vudinhquang <vudinhquangk53@gmail.com>
2020-01-10 00:18:36 +0900

    CartItem

--- ch04/src/components/CartItem.js
+++ ch04/src/components/CartItem.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
+
+import Helpers from './../libs/Helpers';
+
 class CartItem extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            value: 0
         };
     }
 
+    handleChange = (event) => {
+        const target = event.target;    // input selectbox
+        const value  = target.type === 'checkbox' ? target.checked : target.value;
+        const name   = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
     render() {
+        let { cartItem, index } = this.props;
+        let { product } = cartItem;
+        let quantity    = (this.state.value) ? this.state.value : cartItem.quantity;
         return (
             <tr>
-                <th scope="row">1</th>
-                <td>Lorem ipsum.</td>
-                <td>12 USD</td>
-                <td><input name="cart-item-quantity-1" type="number" defaultValue={1} min={1} /></td>
-                <td><strong>12 USD</strong></td>
+                <th scope="row">{ index+1 }</th>
+                <td>{ product.name }</td>
+                <td>{ Helpers.toCurrency(product.price, "$", "left") }</td>
+                <td><input name="value" type="number" value={ quantity } onChange={this.handleChange} min={1} /></td>
+                <td>{ this.showSubTotal(product.price, quantity) }</td>
                 <td>
                 <a className="label label-info update-cart-item" href="#" data-product>Update</a>
                 <a className="label label-danger delete-cart-item" href="#" data-product>Delete</a>
                 </td>
             </tr>
         );
+    }
+
+    showSubTotal(price, quantity){
+        let result = null;
+        result = <strong>{Helpers.toCurrency(price * quantity, "$", "left")} </strong>
+        return result
     }
 }
 
