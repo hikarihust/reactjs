c7d2ccc69aec6d2ef297d1b230464e4585b67caf
vudinhquang <vudinhquangk53@gmail.com>
2020-01-12 02:11:35 +0900

    Link - NavLink

--- ch05/src/components/Menu.js
+++ ch05/src/components/Menu.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Link, NavLink } from "react-router-dom";
 
 class Menu extends Component {
     render() {
         return (
             <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                 <div className="list-group">
-                    <a href="/" className="list-group-item active">Home</a>
-                    <a href="/about" className="list-group-item">About</a>
+                    <NavLink exact to="/" activeClassName="active" className="list-group-item">Home</NavLink>
+                    <NavLink to="/about" activeClassName="active" className="list-group-item">About</NavLink>
                 </div>
             </div>
         );
