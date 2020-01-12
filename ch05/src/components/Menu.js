df8e39ffaa8fcd8605abd8b2d7697efa5fe9992e
vudinhquang <vudinhquangk53@gmail.com>
2020-01-12 16:39:51 +0900

    Menu config

--- ch05/src/components/Menu.js
+++ ch05/src/components/Menu.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import { NavLink } from "react-router-dom";
 
+const menus = [
+    { to: '/'       , exact: true, name: 'Home' },
+    { to: '/about'  , exact: false, name: 'About' },
+    { to: '/course'  , exact: false, name: 'Course' },
+];
+
 class Menu extends Component {
     render() {
         return (
             <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                 <div className="list-group">
-                    <NavLink exact to="/" activeClassName="active" className="list-group-item">Home</NavLink>
-                    <NavLink to="/about" activeClassName="active" className="list-group-item">About</NavLink>
-                    <NavLink to="/course" activeClassName="active" className="list-group-item">Course</NavLink>
+                    { this.showMenus(menus) }
                 </div>
             </div>
         );
     }
+
+    showMenus(menus) {
+        let xhtml = null;
+        
+        if (menus.length > 0) {
+            xhtml = menus.map((menu, index) => {  
+                return (
+                    <NavLink key={ index } exact={ menu.exact } to={ menu.to } activeClassName="active" className="list-group-item">
+                        { menu.name }
+                    </NavLink>
+                );
+            });
+        }
+
+        return xhtml;
+    } 
 }
 
 export default Menu;
