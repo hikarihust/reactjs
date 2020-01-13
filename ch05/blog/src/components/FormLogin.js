a1b277d5b4feaaa9842c7c9edb57516d09f5644b
vudinhquang <vudinhquangk53@gmail.com>
2020-01-13 17:25:40 +0900

    FormLogin

--- ch05/blog/src/components/FormLogin.js
+++ ch05/blog/src/components/FormLogin.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
+import {actLogin} from './../actions/index';
+import {connect} from 'react-redux';
 
 class FormLogin extends Component {
+	constructor(props) {
+        super(props);
+
+        this.state = {
+			username: '',
+			password: ''
+        };
+    }
+
+	handleChange = (event) => {
+        const target = event.target;    // input selectbox
+        const value  = target.type === 'checkbox' ? target.checked : target.value;
+        const name   = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    
+	handleSubmit = (event) => {
+        let {username, password} = this.state;
+		this.props.formLogin(username, password);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="row">
@@ -9,15 +36,20 @@ class FormLogin extends Component {
                         <hr />
                             <h2 className="intro-text text-center">Login <strong>form</strong></h2>
                         <hr />
-                        <form role="form">
+                        <form onSubmit={this.handleSubmit}>
                             <div className="row">
                                 <div className="form-group col-lg-6">
                                     <label>Name</label>
-                                    <input type="text" className="form-control" />
+                                    <input name="username" value={this.state.username} onChange={this.handleChange}  type="text" className="form-control" />
                                 </div>
                                 <div className="form-group col-lg-6">
                                     <label>Password</label>
-                                    <input type="email" className="form-control" />
+                                    <input name="password" value={this.state.password} onChange={this.handleChange} type="text" className="form-control" />
+                                </div>
+
+                                <div className="clearfix" />
+                                <div className="form-group col-lg-12">
+                                    <button type="submit" className="btn btn-primary">Submit</button>
                                 </div>
                             </div>
                         </form>
@@ -28,4 +60,11 @@ class FormLogin extends Component {
     }
 }
 
-export default FormLogin;
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        formLogin: (username, password) => {
+            dispatch(actLogin(username, password)) ;
+        }
+    }
+}
+export default connect(null, mapDispatchToProps)(FormLogin);
