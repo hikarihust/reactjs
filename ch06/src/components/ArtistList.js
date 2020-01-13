bb20917c442a299f63036bd010724a93b24fa34b
vudinhquang <vudinhquangk53@gmail.com>
2020-01-13 20:49:07 +0900

    Query reducer

--- ch06/src/components/ArtistList.js
+++ ch06/src/components/ArtistList.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
 import Artist from './Artist';
 
 class ArtistList extends Component {
     render() {
+        console.log(this.props.query);
         let xhtml = <h3>Enter artist's name to start</h3>
 
         return (
@@ -11,4 +14,10 @@ class ArtistList extends Component {
     }
 }
 
-export default ArtistList;
+const mapStateToProps = state => {
+    return {
+        query: state.query
+    }
+}
+
+export default connect(mapStateToProps, null)(ArtistList);
