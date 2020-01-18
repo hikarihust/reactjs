468d83cd878c16f6f97c49d67c32b7929bb16ca4
vudinhquang <vudinhquangk53@gmail.com>
2020-01-19 00:36:25 +0900

    Tích hợp Firebase vào ứng dụng - Firebase

---
+++ ch07/src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB26187NLv85CHHeXmQDPGvHQ3hWzsqwW4",
+    authDomain: "reactjs-d2869.firebaseapp.com",
+    databaseURL: "https://reactjs-d2869.firebaseio.com",
+    projectId: "reactjs-d2869",
+    storageBucket: "reactjs-d2869.appspot.com",
+    messagingSenderId: "655013396913",
+    appId: "1:655013396913:web:fec201446d22b0004dbd90",
+    measurementId: "G-PL80BNS1FL"
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
