5e0031cb6650e8b9287148d79a2a4eba1c984180
vudinhquang <vudinhquangk53@gmail.com>
2020-01-19 18:02:28 +0900

    List Item

--- ch07/src/components/TaskDoingList.js
+++ ch07/src/components/TaskDoingList.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import FormAddTask from './FormAddTask';
 import TaskDoingItem from './TaskDoingItem';
+import { tasksRef } from './../firebase';
 
 class TaskDoingList extends Component {
+    constructor(props) {
+		super(props);
+
+		this.state = {
+            items: []
+		};
+    }
+
+    UNSAFE_componentWillMount(){
+        tasksRef.on('value', items => {
+            let data = [];
+            items.forEach(item => {
+                const {email, name} = item.val();
+                data.push({email, name, key: item.key} );
+            })
+
+            this.setState({items: data});
+        })
+    }
+
     render() {
+        let { items } = this.state;
+        console.log(items);
         return (                            
             <div className="panel panel-info">
                 <div className="panel-heading">
