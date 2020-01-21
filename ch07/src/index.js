1314caf29f0f3898935424fde865c7e0ded0d0d6
vudinhquang <vudinhquangk53@gmail.com>
2020-01-22 01:09:07 +0900

    Save userinfo

--- ch07/src/index.js
+++ ch07/src/index.js
@@ -3,15 +3,34 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import { firebaseApp } from './firebase';
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import appReducers from './reducers/index';
+
+import {actLogin} from './actions/index';
 
 const store = createStore(
 	appReducers, /* preloadedState, */
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+firebaseApp.auth().onAuthStateChanged(function(user) {
+	if (user) {
+		// User is signed in.
+		let userInfo = {
+			email: user.email,
+			uid: user.uid
+		}
+
+		store.dispatch(actLogin(userInfo));
+	} else {
+	  	// User is signed out.
+	  	console.log('signed out.');
+	}
+});
+console.log(store.getState());
 ReactDOM.render(
 	<Provider store={store}>
         <App />
